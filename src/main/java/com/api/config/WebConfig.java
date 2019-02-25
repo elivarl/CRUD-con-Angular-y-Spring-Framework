155a59372190c7c474eac00621a156a23e958ffb
elivar <elargor@gmail.com>
2019-02-24 22:12:19 -0500

    Creacion proyecto, configuracion com.api.config

---
+++ src/main/java/com/api/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.api.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages= {"com.api.controller"})
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
